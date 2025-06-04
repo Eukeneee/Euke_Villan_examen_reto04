51188a835bbad178bf0a7edecfef9af8b3b4fc02
Eukene Villan Gómez <eukene.villan@alumni.mondragon.edu>
2025-06-04 09:15:43 +0200

    ultima limpieza de datos

--- Limpieza_de_datos.R
+++ Limpieza_de_datos.R
@@ -9,6 +9,8 @@ library(readxl)
 library(smoothmest)
 library(VIM)
 library(plumber)
+
+
 datos<- read.csv("C:/Users/ev269/Downloads/netflix.csv")
 ########## LIMPIEZA DE DATOS ###############
 summary(datos)
@@ -18,3 +20,13 @@ sd_seasons<- sd(datos$Seasons,na.rm=TRUE)
 length(unique(datos$type))
 length(unique(datos$country))
 length(unique(datos$director))
+
+## NA-s variable 
+miss_var_summary(datos)
+# visualizar 
+vis_miss(datos)
+
+# imputar NA-s seasons y minutes
+imput<-kNN (datos, k=5)
+unique(imput ["Seasons"])
+unique(imput ["Minutes"])
